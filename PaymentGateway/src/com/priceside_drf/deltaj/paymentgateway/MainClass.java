1a044b1bb6866111eb306f4e259d5239a25d824a
sulthanftr <sulthanftr@gmail.com>
2024-08-11 21:07:05 +0700

    feat: add PaymentGateway SPL

---
+++ PaymentGateway/src/com/priceside_drf/deltaj/paymentgateway/MainClass.java
@@ -0,0 +1,9 @@
+package com.priceside_drf.deltaj.paymentgateway;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		new PaymentGateway();
+	}
+
+}
---
+++ PaymentGateway/src/com/priceside_drf/deltaj/paymentgateway/PaymentGateway.java
@@ -0,0 +1,9 @@
+package com.priceside_drf.deltaj.paymentgateway;
+
+public class PaymentGateway {
+    boolean processPayment(double amount) {
+        // Basic payment processing logic
+        System.out.println("Processing payment of " + amount);
+        return true;
+    }
+}
